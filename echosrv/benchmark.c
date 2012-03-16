c24d758e9c5dea63e17e1aa7a776d40c86857ec9
zhenpeng(lab121) <zhpeng.is@gmail.com>
2012-03-16 12:55:02 +0800

    expandtab

--- echosrv/benchmark.c
+++ echosrv/benchmark.c
@@ -13,63 +13,63 @@
 
 static inline void err_handle(const char *str)
 {
-	perror(str);
-	exit(1);
+    perror(str);
+    exit(1);
 }
 
 void *echo(void *arg)
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		err_handle("socket");
-	struct sockaddr_in srvaddr;
-	memset(&srvaddr, 0, sizeof(srvaddr));
-	srvaddr.sin_family = AF_INET;
-	srvaddr.sin_port = htons(SRV_PORT);
-	int ret = inet_aton(SRV_ADDRESS, &srvaddr.sin_addr);
-	if (ret == 0)
-		err_handle("inet_ntoa");
-	ret = connect(sockfd, (struct sockaddr*)&srvaddr, sizeof(srvaddr));
-	if (ret != 0)
-		err_handle("connect");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        err_handle("socket");
+    struct sockaddr_in srvaddr;
+    memset(&srvaddr, 0, sizeof(srvaddr));
+    srvaddr.sin_family = AF_INET;
+    srvaddr.sin_port = htons(SRV_PORT);
+    int ret = inet_aton(SRV_ADDRESS, &srvaddr.sin_addr);
+    if (ret == 0)
+        err_handle("inet_ntoa");
+    ret = connect(sockfd, (struct sockaddr*)&srvaddr, sizeof(srvaddr));
+    if (ret != 0)
+        err_handle("connect");
 
-	int count = *(int*)arg;
-	char line[LINE_MAX] = "hellaksjdajlkdajkldajsklda;dladjaldadklsjaldj";
-	size_t len = strlen(line);
+    int count = *(int*)arg;
+    char line[LINE_MAX] = "hellaksjdajlkdajkldajsklda;dladjaldadklsjaldj";
+    size_t len = strlen(line);
 
-	while (count--) {
-		ret = write(sockfd, line, len);
-		if (ret != len) {
-			printf("write error after %d times write.\n", count);
-			exit(-1);
-		}
-		ret = read(sockfd, line, sizeof(line));
-		if (ret <= 0) {
-			printf("read error, after %d times read\n", count);
-			exit(-1);
-		}
-	}
-	return NULL;
+    while (count--) {
+        ret = write(sockfd, line, len);
+        if (ret != len) {
+            printf("write error after %d times write.\n", count);
+            exit(-1);
+        }
+        ret = read(sockfd, line, sizeof(line));
+        if (ret <= 0) {
+            printf("read error, after %d times read\n", count);
+            exit(-1);
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc != 3) return -1;
-	int thd_cnt = atoi(argv[1]);
-	int loop_cnt = atoi(argv[2]);
+    if (argc != 3) return -1;
+    int thd_cnt = atoi(argv[1]);
+    int loop_cnt = atoi(argv[2]);
 
-	pthread_t *thd = (pthread_t *)malloc(thd_cnt * sizeof(pthread_t));
-	if (thd == NULL)
-		err_handle("malloc");
-	int i;
-	for (i = 0; i < thd_cnt; i++) {
-		pthread_create(thd+i, NULL, echo, &loop_cnt);
-	}
+    pthread_t *thd = (pthread_t *)malloc(thd_cnt * sizeof(pthread_t));
+    if (thd == NULL)
+        err_handle("malloc");
+    int i;
+    for (i = 0; i < thd_cnt; i++) {
+        pthread_create(thd+i, NULL, echo, &loop_cnt);
+    }
 
-	for (i = 0; i < thd_cnt; i++) {
-		pthread_join(thd[i], NULL);
-	}
+    for (i = 0; i < thd_cnt; i++) {
+        pthread_join(thd[i], NULL);
+    }
 
-	printf("Done, %d threads, %d loops/thread\n", thd_cnt, loop_cnt);
-	return 0;
+    printf("Done, %d threads, %d loops/thread\n", thd_cnt, loop_cnt);
+    return 0;
 }
--- echosrv/srvmode.c
+++ echosrv/srvmode.c
@@ -17,31 +17,31 @@
 
 int do_accept(int fd)
 {
-	struct sockaddr_in cliaddr;
-	socklen_t addrlen = sizeof(cliaddr);
+    struct sockaddr_in cliaddr;
+    socklen_t addrlen = sizeof(cliaddr);
 
-	int accpfd = accept(fd, (struct sockaddr*)&cliaddr, &addrlen);
-	if (accpfd < 0) {
-		return -1;
-	}
+    int accpfd = accept(fd, (struct sockaddr*)&cliaddr, &addrlen);
+    if (accpfd < 0) {
+        return -1;
+    }
 
-	printf("accept new connection from: %s:%d\n", 
-			inet_ntoa(cliaddr.sin_addr), 
-			ntohs(cliaddr.sin_port));
-	return accpfd;
+    printf("accept new connection from: %s:%d\n", 
+            inet_ntoa(cliaddr.sin_addr), 
+            ntohs(cliaddr.sin_port));
+    return accpfd;
 }
 
 int do_read(int fd)
 {
-	int ret = 0;
-	char buf[1024];
-	if ((ret = read(fd, buf, sizeof(buf))) > 0) {
-		// TODO loop read
-		write(fd, buf, ret);
-	} else {
-		return -1;
-	}
-	return 0;
+    int ret = 0;
+    char buf[1024];
+    if ((ret = read(fd, buf, sizeof(buf))) > 0) {
+        // TODO loop read
+        write(fd, buf, ret);
+    } else {
+        return -1;
+    }
+    return 0;
 }
 
 void echo_select()
@@ -51,42 +51,42 @@ void echo_select()
         return;
     }
 
-	fd_set rdset, backset;
-	FD_ZERO(&rdset);
-	FD_SET(lsnfd, &rdset);
-	backset = rdset;
+    fd_set rdset, backset;
+    FD_ZERO(&rdset);
+    FD_SET(lsnfd, &rdset);
+    backset = rdset;
 
-	struct timeval timeout = {2, 0};
-	int ready, i, maxfd = lsnfd + 1;
-	while ((ready = select(maxfd, &rdset, NULL, NULL, &timeout)) >= 0) {
-		if (ready == 0) {
-			printf("timeout\n");
-		} else {
-			for (i = 0; i < maxfd && ready; i++) {
-				if (FD_ISSET(i, &rdset)) {
-					if (i == lsnfd) {
-						// do accept
-						int acpfd = do_accept(lsnfd);
-						if (acpfd > 0) {
-							FD_SET(acpfd, &backset);
-							if (acpfd + 1 > maxfd)
-								maxfd = acpfd + 1;
-						}
-					} else {
-						// do read
-						int ret = do_read(i);
-						if (ret < 0) {
-							FD_CLR(i, &backset);
-						}
-					}
-					ready--;
-				}
-			}
-		}
-		rdset = backset;
-		timeout.tv_sec = 2;
-		timeout.tv_usec = 0;
-	}
+    struct timeval timeout = {2, 0};
+    int ready, i, maxfd = lsnfd + 1;
+    while ((ready = select(maxfd, &rdset, NULL, NULL, &timeout)) >= 0) {
+        if (ready == 0) {
+            printf("timeout\n");
+        } else {
+            for (i = 0; i < maxfd && ready; i++) {
+                if (FD_ISSET(i, &rdset)) {
+                    if (i == lsnfd) {
+                        // do accept
+                        int acpfd = do_accept(lsnfd);
+                        if (acpfd > 0) {
+                            FD_SET(acpfd, &backset);
+                            if (acpfd + 1 > maxfd)
+                                maxfd = acpfd + 1;
+                        }
+                    } else {
+                        // do read
+                        int ret = do_read(i);
+                        if (ret < 0) {
+                            FD_CLR(i, &backset);
+                        }
+                    }
+                    ready--;
+                }
+            }
+        }
+        rdset = backset;
+        timeout.tv_sec = 2;
+        timeout.tv_usec = 0;
+    }
 }
 
 void echo_poll()
@@ -96,50 +96,50 @@ void echo_poll()
 
 void echo_epoll()
 {
-	int lsnfd = open_socket(LSN_ADDRESS, LSN_PORT);
-	if (lsnfd < 0) return;
+    int lsnfd = open_socket(LSN_ADDRESS, LSN_PORT);
+    if (lsnfd < 0) return;
 
-	int epfd = epoll_create(1024);
-	if (epfd < 0) {
-		perror("epoll_create");
-		return;
-	}
+    int epfd = epoll_create(1024);
+    if (epfd < 0) {
+        perror("epoll_create");
+        return;
+    }
 
-	const int max_events = 1024;
-	struct epoll_event ev, events[max_events];
-	ev.events = EPOLLIN;
-	ev.data.fd = lsnfd;
-	int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, lsnfd, &ev);
-	if (ret != 0) {
-		perror("epoll_ctl");
-		return;
-	}
+    const int max_events = 1024;
+    struct epoll_event ev, events[max_events];
+    ev.events = EPOLLIN;
+    ev.data.fd = lsnfd;
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, lsnfd, &ev);
+    if (ret != 0) {
+        perror("epoll_ctl");
+        return;
+    }
 
-	int i, nfds = 0, timeout = 2000;
-	for (;;) {
-		nfds = epoll_wait(epfd, events, max_events, timeout); 
-		if (nfds < 0) {
-			perror("epoll wait");
-			return;
-		}
-		for (i = 0; i < nfds; i++) {
-			if (events[i].data.fd == lsnfd) {
-				// do accesp
-				int acpfd = do_accept(lsnfd);
-				if (acpfd > 0) {
-					ev.data.fd = acpfd;
-					ev.events = EPOLLIN;
-					epoll_ctl(epfd, EPOLL_CTL_ADD, acpfd, &ev);
-				}
-			} else {
-				// do read
-				int ret = do_read(events[i].data.fd);
-				if (ret < 0) {
-					ev.data.fd = events[i].data.fd;
-					epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, &ev);
-				}
-				// TODO Address alrady in use
-			}
-		}
-	}	
+    int i, nfds = 0, timeout = 2000;
+    for (;;) {
+        nfds = epoll_wait(epfd, events, max_events, timeout); 
+        if (nfds < 0) {
+            perror("epoll wait");
+            return;
+        }
+        for (i = 0; i < nfds; i++) {
+            if (events[i].data.fd == lsnfd) {
+                // do accesp
+                int acpfd = do_accept(lsnfd);
+                if (acpfd > 0) {
+                    ev.data.fd = acpfd;
+                    ev.events = EPOLLIN;
+                    epoll_ctl(epfd, EPOLL_CTL_ADD, acpfd, &ev);
+                }
+            } else {
+                // do read
+                int ret = do_read(events[i].data.fd);
+                if (ret < 0) {
+                    ev.data.fd = events[i].data.fd;
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, &ev);
+                }
+                // TODO Address alrady in use
+            }
+        }
+    }	
 }
