b15dccc8815703d27e754469be2ea3558492a514
zhpengg <zhpeng.is@gmail.com>
2012-11-03 00:12:47 +0800

    add restful test

--- iotmq/broker/lib/IDGenerator.py
+++ iotmq/broker/lib/IDGenerator.py
@@ -11,10 +11,11 @@ class IDGenerator():
 
     def get_id(self):
         now_sec = int(time.time())
+        self.counter += 1
+        if now_sec != self.last_sec or self.counter >= self.max_counter:
+            self.last_sec = now_sec
+            self.counter = 0
+
         time_part= "%d" % now_sec 
         counter_part = "%06d" % self.counter
-
-        self.counter += 1
-        if now_sec != self.last_sec or self.counter > self.max_counter:
-            self.counter = 0
         return time_part + counter_part
---
+++ iotmq/broker/test/test_broker.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REST_SERVER='http://localhost:12321/message/test-topic'
+COUNTER=0
+
+cat ./hosts | while read LINE;
+do
+    curl $REST_SERVER -d data="$LINE"
+    echo "send $COUNTER message"
+    let COUNTER++
+done
